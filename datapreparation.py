14f43e46cf8b1fc3c05c8254f5c76b0446bf3850
Xinyu_Xie <SeanxxY@outlook.com>
2023-11-07 14:53:28 +0100

    rename functions on datapreparation.py

--- datapreparation.py
+++ datapreparation.py
@@ -1,48 +1,40 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset # Dataset is an abstract class, can only be herited
+from OzoneDataset import OzoneDataset
 from torch.utils.data import DataLoader # DataLoader is a class to help us loading data in Pytorch
 import pandas as pd
 
-def sort_data_by_slidingWindow(filepath,col):
+def sort_data(filepath,col):
         odata = pd.read_csv(filepath,usecols=col)
         odata = np.array(odata)
         odata_len = odata.shape[0]
 
-        x_data = []
-        y_data = []
+        X = []
+        y = []
 
         for i in range(odata_len-6):
             _x = odata[i:(i+6),0]
 
-            x_data.append(_x)
-            y_data.append(odata[i+6,0])
+            X.append(_x)
+            y.append(odata[i+6,0])
 
-        x_data = np.array(x_data)
-        y_data = np.array(y_data)
-        return x_data, y_data
+        X = (np.array(X)).astype(np.float32)
+        y = (np.array(y)).astype(np.float32)
+        return X, y
 
-def train_validate_test_data_split(x_data, y_data, train_percentage, validate_percentage):
+def data_split(x_data, y_data, train_percentage, validate_percentage):
      train_size = int(len(x_data)*train_percentage)
      validate_size = int(len(x_data)*validate_percentage)
+
      train_x, train_y = x_data[:train_size],y_data[:train_size]
      validate_x, validate_y = x_data[train_size:train_size+validate_size],y_data[train_size:train_size+validate_size]
      test_x, test_y = x_data[train_size+validate_size:],y_data[train_size+validate_size:]
+
      return train_x, train_y, validate_x, validate_y, test_x, test_y # np.array
 
-class OzoneDataset(Dataset):
-    def __init__(self,x_data,y_data) -> None:
- 
-        self.x_data = torch.from_numpy(x_data)
-        self.y_data = torch.from_numpy(y_data)
-
-    def __len__(self):
-        return self.x_data.shape[0]
     
-    def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
-    
-def dataPrepare(x_data,y_data,batch_size,shuffle=True,num_workers=2):
+def prepare_dataloader(x_data,y_data,batch_size,shuffle=True,num_workers=2):
      dataset = OzoneDataset(x_data=x_data,y_data=y_data)
      dataLoader = DataLoader(
           dataset=dataset,
@@ -53,10 +45,6 @@ def dataPrepare(x_data,y_data,batch_size,shuffle=True,num_workers=2):
      return dataLoader
 
 
-def get_real_y(filepath): # return value type is numpy
-        odata = pd.read_csv(filepath,usecols=[8])
-        odata = np.array(odata)
-        odata_len = len(odata)
-        return odata,odata_len
+
 
 
