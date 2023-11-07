6da31d2fdc6814a187af5c87e57c27d572135ee2
Xinyu_Xie <SeanxxY@outlook.com>
2023-11-07 12:02:52 +0100

    add OzoneDataset.py

---
+++ OzoneDataset.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset # Dataset is an abstract class, can only be herited
+class OzoneDataset(Dataset):
+    def __init__(self,x_data,y_data) -> None:
+ 
+        self.x_data = torch.from_numpy(x_data)
+        self.y_data = torch.from_numpy(y_data)
+
+    def __len__(self):
+        return self.x_data.shape[0]
+    
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
