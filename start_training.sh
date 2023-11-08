638ac4052d3394556d14913aa3d996cfec07fa4d
Xinyu_Xie <SeanxxY@outlook.com>
2023-11-08 17:10:44 +0100

    add a if control

--- start_training.sh
+++ start_training.sh
@@ -1,14 +1,48 @@
 source venv/bin/activate
-python main.py --num_exps=2 \
---is_train \
---filepath=data/data.csv \
---learning_rate=0.01 \
---training_percentage=0.7 \
---validate_percentage=0.1 \
---batch_size=40 \
---input_size=6 \
---hidden_size=10 \
---patience=10 \
---num_of_epochs=100 \
---lossfunction="MSE"
+is_train=true \
+num_exps=2 \
+filepath=data/data.csv \
+learning_rate=0.01 \
+training_percentage=0.7 \
+validate_percentage=0.1 \
+batch_size=40 \
+input_size=6 \
+hidden_size=10 \
+patience=10 \
+num_of_epochs=100 \
+lossfunction="MSE" \
+model_folder_dir="./models" \
+plot_folder_dir="./pic"
+if [ "$is_train" = true ]; then
+   echo 'training, validation and test'
+   python main.py --num_exps=$num_exps \
+    --is_train \
+    --filepath=$filepath \
+    --learning_rate=$learning_rate \
+    --training_percentage=$training_percentage \
+    --validate_percentage=$validate_percentage \
+    --batch_size=$batch_size \
+    --input_size=$input_size \
+    --hidden_size=$hidden_size \
+    --patience=$patience \
+    --num_of_epochs=$num_of_epochs \
+    --lossfunction=$lossfunction \
+    --model_folder_dir=$model_folder_dir \
+    --plot_folder_dir=$plot_folder_dir
+else
+    echo 'only conduct testing'
+    python main.py --num_exps=$num_exps \
+    --filepath=$filepath \
+    --learning_rate=$learning_rate \
+    --training_percentage=$training_percentage \
+    --validate_percentage=$validate_percentage \
+    --batch_size=$batch_size \
+    --input_size=$input_size \
+    --hidden_size=$hidden_size \
+    --patience=$patience \
+    --num_of_epochs=$num_of_epochs \
+    --lossfunction=$lossfunction \
+    --model_folder_dir=$model_folder_dir \
+    --plot_folder_dir=$plot_folder_dir
+fi
 deactivate
