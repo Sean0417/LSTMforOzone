5057161585158b15846cea0d9e63001edb5d6665
Xinyu_Xie <SeanxxY@outlook.com>
2023-11-08 16:57:47 +0100

    change the data preparation

--- evaluation.py
+++ evaluation.py
@@ -1,33 +1,40 @@
 import torch
 import numpy as np
 import wandb
+import sys
 
-def evaluation(model,test_loader,lossfunction):
+def evaluation(model,test_loader,lossfunction,filepath):
+
     test_loss_sum = 9
-    n = 1
     predictions = []
-    labels = []
-    model.load_state_dict(torch.load('models/best_model.pkl'))
+    targets = []
+    model.load_state_dict(torch.load(filepath))
+   
     model.eval()
+
     if lossfunction == 'MSE':
         criterion = torch.nn.MSELoss(reduction="mean")
     elif lossfunction == 'MAE':
         criterion = torch.nn.L1Loss(size_average=False)
+    else:
+        print("Please select between MSE and MAE")
+        sys.exit(1)
+
     with torch.no_grad():
         for i,data in enumerate(test_loader):
             x, y = data
-            y = torch.tensor(y,dtype=torch.float32).view(-1,1)
-            x = torch.tensor(x, dtype=torch.float32).view(-1,1,6)
+            y = y.view(-1,1)
+            x = x.view(-1,1,6)
             y_pred = model(x)
 
             test_loss = criterion(y_pred,y).item()
             test_loss_sum +=test_loss
-            n +=1
             y_pred = y_pred.numpy()
             y = y.data.numpy()
             predictions.extend(y_pred)
-            labels.extend(y)
-            wandb.log({"test_loss":test_loss})
-        test_loss = test_loss_sum/n
-    return labels, predictions, test_loss # labels and predictions are lists, each element is a numpy dtype=float32
+            targets.extend(y)
+
+        avg_test_loss = test_loss_sum/len(targets)
+
+    return targets, predictions, avg_test_loss # labels and predictions are lists, each element is a numpy dtype=float32
     
